c9455bc968939fd5d7e364cb71a8e192723b827a
pluto4planet <michael.t98@t-online.de>
2020-10-04 10:01:07 +0200

    AAAAAAAAAAAA

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,8 @@
     <h1 class="display-4">Warum sind SPiegel so hässlich?</h1>
     <v-btn class="pink white--text">kys</v-btn>
     <v-btn depressed color="pink">click me</v-btn>
-    <v-btn flat color="pink">click me</v-btn>
-    <v-btn depressed class="pink white--text">
+    <v-btn text color="pink">click me</v-btn>
+    <v-btn @click="doSomething" class="pink white--text">
       <v-icon left>email</v-icon>
       <span>kys piss</span>
     </v-btn>
@@ -14,9 +14,17 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+const curseforge = require("mc-curseforge-api");
 
-@Component({
-  
-})
-export default class Home extends Vue {}
+@Component({})
+export default class Home extends Vue {
+  doSomething = async () => {
+    console.log("Button click");
+    console.log("AAAAAAB");
+    const mods = await curseforge.getMods({
+      searchFilter: "applied energistics"
+    });
+    console.log(mods);
+  };
+}
 </script>
